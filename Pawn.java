bf353ebfac4a08cbb260cd158f0d9ea7a3f21be6
jsputnik <65297977+jsputnik@users.noreply.github.com>
2020-05-21 22:48:10 +0200

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -2,22 +2,26 @@ package com.checkers;
 
 public class Pawn {
         private boolean colour; //false - black, true - white
-        protected int lives;
+        private int lives;
+        private boolean type; //false - pawn, true - king
         //private int state; //not neccessary
         //empty field
         public Pawn() {
             colour = false; //not important
-            lives = 0;
+            lives = 0; //not important
+            type = false; //not important
         }
-        //put new pawn on the field
-        public Pawn(boolean c) {
+        //put pawn on the field
+        public Pawn(boolean c, boolean t) {
             colour = c;
             lives = 2;
+            type = t;
         }
         //copy constructor
         public Pawn(Pawn pawn) {
             colour = pawn.colour;
             lives = pawn.lives;
+            type = pawn.type;
         }
 
         public boolean getColour() {
@@ -27,6 +31,8 @@ public class Pawn {
         public int getLives() {
             return lives;
         }
+
+        public boolean getType() { return type; }
 
         public boolean remLife() {
             --lives;
@@ -40,5 +46,8 @@ public class Pawn {
         public void remPawn() {
             lives = 0;
         }
-}
 
+        public void convertToKing() {
+            type = true;
+        }
+}
