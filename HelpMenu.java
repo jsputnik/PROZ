347dcffc84590742da81dddfba47e755f30472ef
jsputnik <65297977+jsputnik@users.noreply.github.com>
2020-06-13 17:20:39 +0200

    dodalem nowa zasade xd

--- HelpMenu.java
+++ HelpMenu.java
@@ -21,8 +21,9 @@ public class HelpMenu extends JFrame implements ActionListener {
         helpText.setEditable(false);
         helpText.setContentType("text/html");
         helpText.setBackground(Color.orange);
-        helpText.setText("Zasady:<br><ol><li>ruchy do przodu lub w boki po 1 polu<li>zbijać można tylko po ukosie<li>pionki mają po 2 życia<li>damka znika po 5 ruchach" +
-                "<li>dostępne 3 tryby gry:<ul><li>plansza 6x6, 2x12 pionków, 3 ożywienia<li>plansza 8x8, 2x16 pionków, 4 ożywienia<li>plansza 10x10, 2x20 pionków, 5 ożywień");
+        helpText.setText("Zasady:<br><ol><li>ruchy do przodu lub w boki po 1 polu<li>zbijać można tylko po ukosie max 1 pionek<li>pionki mają po 2 życia" +
+                "<li>damka znika po 5 ruchach<li>dostępne 3 tryby gry:<ul><li>plansza 6x6, 2x12 pionków, 3 ożywienia<li>plansza 8x8, 2x16 pionków," +
+                "4 ożywienia<li>plansza 10x10, 2x20 pionków, 5 ożywień");
         add(helpText, BorderLayout.NORTH);
         add(back, BorderLayout.SOUTH);
         setVisible(true);
