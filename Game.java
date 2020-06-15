f2a6e4cf2dbb854599afb2cc0a6e586246d4dc92
jsputnik <65297977+jsputnik@users.noreply.github.com>
2020-06-15 18:17:44 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,22 +1,13 @@
-package com.checkers;
+package com.mycompany.app;
 import java.awt.EventQueue;
 
 public class Game {
-
     public static void main(String[] args) {
         System.out.println("Start");
         EventQueue.invokeLater(new Runnable() {
             public void run() {
-                ModelUnitTest uTest = new ModelUnitTest(1);
-                //uTest.test1();
-                //uTest.test2();
-                //uTest.test3();
-                //uTest.test4();
-                //new StartMenu();
-                
+                new StartMenu();
             }
         });
     }
-
-
 }
