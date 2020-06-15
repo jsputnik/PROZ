af5b53bee940d68153cf4d188463b46b1deb2bcd
jsputnik <65297977+jsputnik@users.noreply.github.com>
2020-06-15 14:22:43 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -2,21 +2,21 @@ package com.checkers;
 import java.awt.EventQueue;
 
 public class Game {
-    private Player one;
-    private Player two;
 
     public static void main(String[] args) {
         System.out.println("Start");
         EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new StartMenu();
+                ModelUnitTest uTest = new ModelUnitTest(1);
+                //uTest.test1();
+                //uTest.test2();
+                //uTest.test3();
+                //uTest.test4();
+                //new StartMenu();
+                
             }
         });
     }
-    //public Game {
-        //one = new Player();
-        //two = new Player();
-    //}
 
 
 }
